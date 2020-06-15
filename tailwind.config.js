a216aa93a76f571008eeefc0a83f4c4ada538a95
Nguyen Tuan Cuong <cuong.nguyentuan@digia.com>
2020-06-15 22:51:27 +0300

    init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: false,
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Nunito Sans', 'sans-serif'],
+        mono: ['Inconsolata', 'monospace'],
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
